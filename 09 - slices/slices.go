4b3a21492713303c82037f01703a63d8b89c6cac
alanloffler <alanmatiasloffler@gmail.com>
2024-07-29 11:05:24 -0300

    Slides module

--- 09 - slices/slices.go
+++ 09 - slices/slices.go
@@ -32,4 +32,29 @@ func main() {
 	c := make([]string, len(s))
 	copy(c, s)
 	fmt.Println("cpy:", c)
+
+	// Slice operations
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	// Declare and initialize
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	// Composed multi-dimensional slides
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
 }
