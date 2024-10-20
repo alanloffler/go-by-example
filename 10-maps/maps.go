cda9df14e850b0a169e8a613027dcf565077153d
alanloffler <alanmatiasloffler@gmail.com>
2024-10-20 11:14:07 -0300

    Advanced until delete

--- 10-maps/maps.go
+++ 10-maps/maps.go
@@ -3,10 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	// Create empty map
 	m := make(map[string]int)
 
+	// Set values
 	m["k1"] = 7
 	m["k2"] = 13
+	fmt.Println("map:", m)
 
+	// Get value by key
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	// Zero value for non-existent key
+	v3 := m["k3"]
+	fmt.Println("v3:", v3)
+
+	// Lenght of keys/values
+	fmt.Println("len:", len(m))
+
+	// Remove key/value
+	delete(m, "k2")
 	fmt.Println("map:", m)
 }
